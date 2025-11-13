34b6ee43c0f3f34905ad7d600f5a95ae7dbe790b
myhrelin <77819233+myhrelin@users.noreply.github.com>
2025-11-13 10:18:37 +0700

    Add files via upload

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+	'key' : 'f673a257968e3d9f0b81dbffd36e1338',
+	'format' : 'iframe',
+	'height' : 90,
+	'width' : 728,
+	'params' : {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//beliefnormandygarbage.com/f673a257968e3d9f0b81dbffd36e1338/invoke.js"></scr' + 'ipt>');
