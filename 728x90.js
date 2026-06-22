8d8610ed54b4a64f3d56e9222b52de9df514f2c5
myhrelin <77819233+myhrelin@users.noreply.github.com>
2026-06-22 17:33:18 +0700

    Update 728x90.js

--- 728x90.js
+++ 728x90.js
@@ -1,8 +1,8 @@
 atOptions = {
-	'key' : 'f673a257968e3d9f0b81dbffd36e1338',
+	'key' : '10f7c73880db5e301c5c9f8eea64774e',
 	'format' : 'iframe',
 	'height' : 90,
 	'width' : 728,
 	'params' : {}
 };
-document.write('<scr' + 'ipt type="text/javascript" src="//beliefnormandygarbage.com/f673a257968e3d9f0b81dbffd36e1338/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt src="//beliefnormandygarbage.com/10f7c73880db5e301c5c9f8eea64774e/invoke.js"></scr' + 'ipt>');
